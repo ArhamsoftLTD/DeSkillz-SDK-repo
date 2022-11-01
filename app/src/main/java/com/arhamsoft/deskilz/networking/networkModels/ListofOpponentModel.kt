7ba2e53d3fad518ad1026c9bf954e244a24bb30d
mujahids2244 <111061767+mujahids2244@users.noreply.github.com>
2022-11-01 16:23:40 +0500

    SDK library
    done

---
+++ app/src/main/java/com/arhamsoft/deskilz/networking/networkModels/ListofOpponentModel.kt
@@ -0,0 +1,10 @@
+package com.arhamsoft.deskilz.networking.networkModels
+
+data class ListofOpponentModel(
+    val opponentImage: String?="",
+
+    val opponentId: String? ="",
+
+    val opponentName: String? = "",
+    val isLeave:Boolean = false
+)
