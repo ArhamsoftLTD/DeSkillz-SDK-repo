f6fc31929a46eea3a6a9ad1a1884f5f9c9fa8e3d
arhamsoft <arhamsoftltd@gmail.com>
2022-12-06 19:18:43 +0500

    update response of event
    done

--- app/src/main/java/com/arhamsoft/deskilz/networking/networkModels/GetRandomPlayerModelData.kt
+++ app/src/main/java/com/arhamsoft/deskilz/networking/networkModels/GetRandomPlayerModelData.kt
@@ -3,7 +3,8 @@ package com.arhamsoft.deskilz.networking.networkModels
 data class GetRandomPlayerModelData(
     val matchID: String,
     val gameImage: String,
-    val gameGenre: String,
+    val gameGenre: List<GameGenre>,
+
     val gameInstructions: String,
     val gamePlatform: Long,
 
